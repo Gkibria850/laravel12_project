e027f2b8a5bf28f99957d48cf4fa13af2074177b
kibria <gkibria850@gmail.com>
2025-03-23 00:12:13 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"  />
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="wrapper">
+            <div class="title">
+                <span>Welcome to Our website!</span>
+            </div>
+            <form>
+               
+                <div class="signup-link">Sign In?<a>Login</a></div>
+                <div class="signup-link">Join Now <a>Registration</a></div>
+                
+            </form>
+        </div>
+    </div>
+    
+</body>
+</html>
