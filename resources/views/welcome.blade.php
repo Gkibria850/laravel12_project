475d7e0ba1ea25cf99e96c85aa86c8c03d6ee9c3
kibria <gkibria850@gmail.com>
2025-03-30 14:09:41 +0600

    first7 commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,10 +9,22 @@
                 <span>Welcome to Our website!</span>
             </div>
             <form>
+                @if(Auth::check())
+
+                @if(Auth::user()->is_role == 2)
+
+                <div class="signup-link">Super Admin Dashboard Already Login  <a href="{{url('superadmin/dashboard')}}"> Dashboard</a></div>
+                @elseif(Auth::user()->is_role == 1)
+                <div class="signup-link">Admin Dashboard Already Login  <a href="{{url('admin/dashboard')}}"> Dashboard</a></div>
+                @elseif(Auth::user()->is_role == 0)
+                <div class="signup-link">User Dashboard Already Login  <a href="{{url('user/dashboard')}}"> Dashboard</a></div>
+                @endif
+
+                @else
                
                 <div class="signup-link">Sign In?<a href="{{url('login')}}">Login</a></div>
                 <div class="signup-link">Join Now <a href="{{url('registration')}}">Registration</a></div>
-                
+                @endif
             </form>
         </div>
     </div>
