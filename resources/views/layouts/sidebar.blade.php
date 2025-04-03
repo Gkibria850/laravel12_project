900caff0a7c7e5f33ceb8c62d8e70457ac7bc05a
kibria <gkibria850@gmail.com>
2025-04-03 17:45:47 +0600

    third2 commit

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,36 @@
+<div class="sidebar">
+    <h4>Super Admin MGK CODING</h4>
+    <ul class="nav flex-column">
+        <li class="nav-item">
+            <a href="#" class="nav-link active">
+                <i class="fa fa-user"></i> Dashboard   
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="fa fa-user"></i> Profile 
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">
+                <i class="fa fa-list"></i> List User 
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="{{url('logout')}}" class="nav-link">
+                <i class="fa fa-sign-out-alt"></i> Logout  
+            </a>
+        </li>
+    </ul>
+</div>
+   
+<div class="content">
+    <nav class="navbar">
+        <span>Super Admin</span>
+        <a href="#">
+            <i class="fa fa-user"></i> Profile 
+        </a> 
+    </nav>
+
+    <div class="container mt-4">
+        <div class="row">
